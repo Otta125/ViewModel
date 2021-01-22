230400be8463aa12c862f10c20fdcd4c31b8a4d8
Hussein <trabizaapp@gmail.com>
2021-01-22 17:29:13 +0200

    Initial commit

---
+++ app/src/main/java/com/otherlogic/revision/MainActivity.java
@@ -0,0 +1,18 @@
+package com.otherlogic.revision;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container_a, new FragmentA())
+                .add(R.id.container_b, new FragmentB())
+                .commit();
+    }
+}
